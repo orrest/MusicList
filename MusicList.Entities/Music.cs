07cebfc0e258db68a54e0ba34f283e202a448f7f
OR <zzhxufeng@163.com>
2025-11-30 22:01:26 +0800

    Add test.

---
+++ MusicList.Entities/Music.cs
@@ -0,0 +1,28 @@
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+// ReSharper disable ExplicitCallerInfoArgument
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+
+namespace MusicList.Entities;
+
+[Table("music")]
+public class Music : BaseModel
+{
+    [PrimaryKey("id")]
+    public string Id { get; set; }
+
+    [Column("title")]
+    public string Title { get; set; }
+
+    [Column("artist")]
+    public string Artist { get; set; }
+
+    [Column("description")]
+    public string Description { get; set; }
+
+    [Column("file_name")]
+    public string FileName { get; set; }
+
+    [Column("duration")]
+    public double Duration { get; set; }
+}
---
+++ MusicList.Test/FetchTest.cs
@@ -0,0 +1,27 @@
+﻿using MusicList.Entities;
+using Supabase;
+
+namespace MusicList.Test;
+
+[TestClass]
+public sealed class FetchTest
+{
+    [TestMethod]
+    public async Task FetchMusicList()
+    {
+        const string publishedKey = "sb_publishable_fBKjSpOOMz7zx0Vpqz_CUw_sHbBVUg2";
+        const string url = "https://xsnqymjedaysxqrweylv.supabase.co";
+
+        var client = new Client(url, publishedKey);
+        await client.InitializeAsync();
+        var response = await client.From<Music>().Get();
+        var musicList = response.Models;
+        
+        Assert.IsNotNull(musicList);
+        Assert.IsTrue(musicList.Count > 0);
+        
+        var music = musicList.FirstOrDefault();
+        Assert.IsNotNull(music);
+    }
+
+}
